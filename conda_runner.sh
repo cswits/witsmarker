eccdce95dea2e476e517a4a7fb3a6f8c30ad32a5
Richard Klein <kleinric@gmail.com>
2022-09-12 12:44:09 +0200

    Commit old changed

---
+++ conda_runner.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+eval "$(/anaconda/bin/conda shell.bash hook)"
+python $@ <&0 2>&2 >&1
---
+++ packages.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt install apache2 libapache2-mod-php php-yaml php-zip php-curl build-essential openjdk-16-jdk-headless python3 unzip zip python3-numpy python3-scipy
